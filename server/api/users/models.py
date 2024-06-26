32dad77ec512aade67e3cd82afd0454108e4847d
Divrr <96827300+Divrr@users.noreply.github.com>
2024-06-26 12:26:59 +0000

    added the necessary fields to AbstractUser

--- server/api/users/models.py
+++ server/api/users/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ATTENDEE = 'attendee'
+        POSTER = 'poster'
+        ADMIN = 'admin'
+
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=200, unique=True)
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    password = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now_add=True)
+    role = models.CharField(
+        max_length=8,
+        choices=Role.choices,
+        default=Role.ATTENDEE
+    )
+
+    def __str__(self):
+        return self.username
