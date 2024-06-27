8dc3b700a7bbfe6c12ae105d340252ed6634d36b
Jason Keo <jasonkeo047@outlook.com>
2024-06-27 06:57:56 +0000

    added token_verfiyview

--- server/api/auth/urls.py
+++ server/api/auth/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView
 )
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
-
