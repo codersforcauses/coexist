dff597dc48d709184d79a076e154dc95b776462b
Divrr <96827300+Divrr@users.noreply.github.com>
2024-06-27 02:21:05 +0000

    Added create_user and create_superuser methods to new UserManager

--- server/api/users/models.py
+++ server/api/users/models.py
@@ -1,8 +1,40 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
 
 
-class User(AbstractUser):
+class UserManager(BaseUserManager):
+    def _create_user(self, username, email, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not username:
+            raise ValueError("The given username must be set")
+
+        email = self.normalize_email(email)
+        username = User.normalize_username(username)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(username, email, password, **extra_fields)
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(username, email, password, **extra_fields)
+
+
+class User(AbstractUser, PermissionsMixin):
     class Role(models.TextChoices):
         ATTENDEE = 'attendee'
         POSTER = 'poster'
@@ -21,6 +53,10 @@ class User(AbstractUser):
         choices=Role.choices,
         default=Role.ATTENDEE
     )
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['password']
+    objects = UserManager()
 
     def __str__(self):
         return self.username
