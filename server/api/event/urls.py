a96f58e929a6f243d24c01dbbc9bf7924fce5d1e
Harry Rigg <riggh@icloud.com>
2024-06-29 14:52:42 +0800

    Setup basic Event view

--- server/api/event/urls.py
+++ server/api/event/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register(r"", views.EventViewSet, basename="event")
+
 urlpatterns = [
-    path("<int:event_id>/", views.get_event),
+    path("", include(router.urls))
 ]
