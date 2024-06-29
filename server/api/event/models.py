31ae1d624491f15fc24a6d503610651115c15b2f
Harry Rigg <riggh@icloud.com>
2024-06-29 05:36:11 +0000

    Update Event model

--- server/api/event/models.py
+++ server/api/event/models.py
@@ -3,12 +3,16 @@ from django.db import models
 
 class Event(models.Model):
     created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=200)
     description = models.TextField()
-    image = models.ImageField()
+    # TODO: Install Pillow
+    # image = models.ImageField()
     date_time = models.DateTimeField()
     location = models.CharField(max_length=200)
-    branch = models.ForeignKey("Branch", on_delete=models.CASCADE)
+    # TODO: Create Branch model
+    # branch = models.ForeignKey(
+    #     "Branch", on_delete=models.CASCADE, related_name="events")
     is_cancelled = models.BooleanField(default=False)
 
     def __str__(self):
