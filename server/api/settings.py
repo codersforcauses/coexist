ad70de592761b61db75e622e3c5cc371bcfe705c
Connor2803 <87635341+Connor2803@users.noreply.github.com>
2024-06-29 08:37:32 +0000

    Revert test commit

--- server/api/settings.py
+++ server/api/settings.py
@@ -55,7 +55,6 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
 ]
 
-# tet
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
