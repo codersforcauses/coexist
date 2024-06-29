9d93d47cdcde837c982aaf84471f3be2bdada832
Ellen Xiao <ellenxiao0412@gmail.com>
2024-06-29 07:39:09 +0000

    modal basic structure

---
+++ client/src/components/main/EditProfileModal.tsx
@@ -0,0 +1,44 @@
+import { Copy } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+export default function EditProfileModal() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Share</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle>Share link</DialogTitle>
+          <DialogDescription>
+            Anyone who has this link will be able to view this.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="flex items-center space-x-2">
+          <Button type="submit" size="sm" className="px-3">
+            <span className="sr-only">Copy</span>
+            <Copy className="h-4 w-4" />
+          </Button>
+        </div>
+        <DialogFooter className="sm:justify-start">
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              Close
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
---
+++ client/src/pages/test-page.tsx
@@ -0,0 +1,43 @@
+import { Work_Sans as FontSans } from "next/font/google";
+import { useState } from "react";
+
+import EditProfileModal from "@/components/main/EditProfileModal";
+import Header from "@/components/main/Header";
+import { usePings } from "@/hooks/pings";
+import { cn } from "@/lib/utils";
+
+import { Button } from "../components/ui/button";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export default function Home() {
+  const [clicked, setClicked] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const { data, isLoading } = usePings({
+    enabled: clicked,
+  });
+
+  const onClickHandler = () => {
+    showModal ? setShowModal(false) : setShowModal(true);
+    setClicked(true);
+  };
+
+  return (
+    <main
+      className={cn(
+        "m-0 flex min-h-screen flex-col items-center p-0 font-sans",
+        fontSans.variable,
+      )}
+    >
+      <Header />
+
+      <h1 className="text-3xl text-primary">Test title</h1>
+      <Button onClick={onClickHandler}>{isLoading ? "Loading" : "Ping"}</Button>
+
+      {showModal && <EditProfileModal />}
+    </main>
+  );
+}
