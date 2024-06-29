fcddd4ab40e4e882fa0fc9589a0872a788352286
Divrr <96827300+Divrr@users.noreply.github.com>
2024-06-29 06:53:45 +0000

    added admin class and forms

--- server/api/users/admin.py
+++ server/api/users/admin.py
@@ -1,4 +1,34 @@
 from django.contrib import admin
 from .models import User
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ("email", "is_staff", "is_active",)
+    list_filter = ("email", "is_staff", "is_active",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": (
+            "is_staff",
+            "is_active",
+            "groups",
+            "user_permissions"
+        )}),
+    )
+    add_fieldsets = ((
+            None, {
+                "classes": ("wide",),
+                "fields": (
+                    "email", "password1", "password2", "is_staff",
+                    "is_active", "groups", "user_permissions"
+                )
+            }
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
