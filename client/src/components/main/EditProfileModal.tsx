610bf6f747dfc696857fb5cd16be182144ca63e2
Kat <106138621+KKatariah@users.noreply.github.com>
2024-07-01 15:33:09 +0800

    rounded text boxes and placed divider

--- client/src/components/main/EditProfileModal.tsx
+++ client/src/components/main/EditProfileModal.tsx
@@ -22,19 +22,44 @@ export default function EditProfileModal() {
           <FaRegEdit></FaRegEdit>
         </Button>
       </DialogTrigger>
-      <DialogContent className="w-11/12 rounded-lg bg-blue-300 md:max-2xl:w-96">
+      <DialogContent className="md:max-2xl:w-96rem w-11/12 rounded-lg bg-blue-300">
         <DialogHeader>
           <DialogTitle>Edit Profile</DialogTitle>
-          {/* <DialogDescription>
-            Anyone who has this link will be able to view this.
-          </DialogDescription> */}
+          <div className="border-t border-t-black"></div>
         </DialogHeader>
-        <div className="h-28 w-full place-self-center bg-pink-400">
-          <form>
-            <label htmlFor="fname">First name:</label>
-            <input type="text" id="fname" name="fname" />
-          </form>
+        <div>
+          <a className="">Change Profile Picture</a>
         </div>
+        <form className="w-full space-y-4 bg-white p-8">
+          <div>
+            <label className="" htmlFor="fname">
+              First name
+            </label>
+            <input
+              className="rounded-lg bg-red-700"
+              type="text"
+              id="fname"
+              name="fname"
+            />
+          </div>
+          <div>
+            <label htmlFor="lname">Last name</label>
+            <input type="text" id="lname" name="lname" />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input type="text" id="email" name="email" />
+          </div>
+          <div>
+            <label htmlFor="city">City</label>
+            <input type="text" id="city" name="city" />
+          </div>
+          <div>
+            <label htmlFor="fname">Old Password</label>
+            <input type="password" id="oldpassword" name="oldpassword" />
+          </div>
+        </form>
+
         {/* <DialogFooter className="sm:justify-start">
           <DialogClose asChild>
             <Button type="button" variant="secondary">
