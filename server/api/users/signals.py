3d67bed4a696eb3183183a231b92f080384217ad
Joel <rajujoel403@gmail.com>
2024-07-03 14:55:24 +0000

    added line for signals

--- server/api/users/signals.py
+++ server/api/users/signals.py
@@ -3,6 +3,7 @@ from django.db.models.signals import post_save
 from .models import CustomUser
 from django.contrib.auth.models import User, Group
 
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
