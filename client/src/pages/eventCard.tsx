462a843dd3199462c0955f976846070c3c4d2cbb
CarrotsCode <126574850+CarrotsCode@users.noreply.github.com>
2024-07-03 08:09:04 +0000

    added proof of concept event card template

--- client/src/pages/eventCard.tsx
+++ client/src/pages/eventCard.tsx
@@ -9,7 +9,6 @@ import { Button } from "../components/ui/button";
 // proof of concept event card display, eventually needs to be converted into tailwind component
 // Mostly to show alignments
 // TODO: specific stylings including colour, and precise space allignment
-// Current Issues: flex-grow not working properly, all columns
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -25,13 +24,13 @@ export default function Home() {
   return (
     <main
       className={cn(
-        "flex flex-row items-start gap-4 bg-gray-300 p-24 font-sans",
+        "flex min-h-screen flex-row items-start gap-4 p-24 font-sans",
         fontSans.variable,
       )}
     >
       {/* Column1, need to figure out  */}
-      <div className="w-1/8 order-1 h-full flex-col border border-black p-6 text-primary">
-        <div className="order-1 h-1/4 w-1/4 justify-self-start border-b border-gray-300 p-2 font-bold">
+      <div className="w-1/8 order-1 flex-col p-6 text-primary">
+        <div className="order-1 h-1/4 w-1/4 justify-self-start border-b border-gray-300 p-2">
           May
         </div>
         <div className="order-2 h-1/2 justify-self-center p-2">01</div>
@@ -41,8 +40,8 @@ export default function Home() {
       </div>
 
       {/* Column2 */}
-      <div className="w-5/8 order-2 h-full flex-col border border-black p-6 text-primary">
-        <div className="w-5/8 order-1 h-1/4 justify-self-start p-2 font-bold">
+      <div className="w-5/8 order-2 flex-col p-6 text-primary">
+        <div className="w-5/8 order-1 h-1/4 justify-self-start p-2">
           Tree Planting & Social Swim
         </div>
         <div className="h-1/8 order-2 flex-row border-b border-gray-300 p-2">
@@ -61,7 +60,7 @@ export default function Home() {
       </div>
 
       {/* Column3, To be replaced w image */}
-      <div className="roundedlg order-3 h-full w-1/4 border border-black bg-green-400 p-6"></div>
+      <div className="order 3 roundedlg w-1/4 bg-green-400 p-6"></div>
     </main>
   );
 }
