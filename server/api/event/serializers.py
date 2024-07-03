b2d8ef8b941df235cf5e4e20f204e0538d164303
Joel <rajujoel403@gmail.com>
2024-07-03 13:57:15 +0000

    updated serializer.py for RSVP

--- server/api/event/serializers.py
+++ server/api/event/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Event
+from .models import RSVP
 
 
 class EventSerializer(serializers.ModelSerializer):
@@ -7,3 +8,9 @@ class EventSerializer(serializers.ModelSerializer):
         model = Event
         fields = "__all__"
         read_only_fields = ("id", "created_time", "updated_time")
+
+
+class RSVPSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RSVP
+        fields = ['id', 'user', 'event', 'status', 'created_at', 'updated_at']
