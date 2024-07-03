1c60d46b2fc334c59eea0b9b5b9911585948773c
SetroZ <joemosabry@gmail.com>
2024-07-03 12:29:22 +0800

    made migrations

--- server/api/branch/apps.py
+++ server/api/branch/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class BranchConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'branch'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "api.branch"
--- server/api/branch/models.py
+++ server/api/branch/models.py
@@ -3,13 +3,13 @@ from api.baseModels import SoftDeleteModel
 
 # Create your models here.
 
+
 # One Branch to Many events.
-#Field: Events
+# Field: Events
+
 class Branch(SoftDeleteModel):
     name = models.CharField(max_length=200)
     description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
+    
     def __str__(self):
-        return self.title
-
+        return self.name
