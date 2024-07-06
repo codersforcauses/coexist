848457c01f6c4bd6c4891049b568c8cf4faa163b
Harry Rigg <riggh@icloud.com>
2024-07-06 13:39:15 +0800

    Rewrite shadcn dialog with better styling

--- client/src/components/main/RsvpListModal.tsx
+++ client/src/components/main/RsvpListModal.tsx
@@ -1,7 +1,13 @@
-import { Users, X } from "lucide-react";
-import { useRef, useState } from "react";
+import { Users } from "lucide-react";
 
-import { Button } from "../ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/BetterDialog";
+import { Button } from "@/components/ui/button";
 import {
   Table,
   TableBody,
@@ -9,23 +15,9 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "../ui/table";
+} from "@/components/ui/table";
 
 export default function RsvpListModal() {
-  const [show, setShow] = useState(false);
-
-  const background = useRef(null);
-
-  function toggle() {
-    setShow((v) => !v);
-  }
-
-  function clickOff(e: any) {
-    if (e.target === background.current) {
-      toggle();
-    }
-  }
-
   const attendees = [
     {
       firstName: "John",
@@ -80,58 +72,41 @@ export default function RsvpListModal() {
   ];
 
   return (
-    <>
-      <Button variant="outline" onClick={toggle}>
-        Open Dialog
-      </Button>
-      <div
-        className={`fixed left-0 top-0 flex h-full w-full items-center justify-center bg-neutral-600/50 ${show ? "block" : "hidden"}`}
-        ref={background}
-        onClick={clickOff}
-      >
-        <div
-          className={`flex h-full w-full flex-col gap-3 rounded bg-neutral-50 p-4 md:h-4/5 md:w-4/5 lg:h-3/5 lg:w-3/5`}
-        >
-          <div className="flex items-center justify-between py-1">
-            <div className="flex flex-col gap-4 md:flex-row">
-              <h1 className="text-lg font-semibold leading-none tracking-tight">
-                RSVPs for Tree Planting & Social Swim
-              </h1>
-              <div className="flex items-center gap-1">
-                <Users size="18" />
-                <span className="text-sm text-neutral-500">
-                  {attendees.length}
-                </span>
-              </div>
+    <Dialog>
+      <DialogTrigger>
+        <Button variant="outline">Show RSVPs</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <div className="flex flex-col gap-4 md:flex-row">
+            <DialogTitle>RSVPs for Tree Planting & Social Swim</DialogTitle>
+            <div className="flex items-center gap-1">
+              <Users size="18" />
+              <span className="text-sm text-neutral-500">
+                {attendees.length}
+              </span>
             </div>
-            <button onClick={toggle}>
-              <X className="h-7 w-7"></X>
-            </button>
           </div>
-          <div className="overflow-y-auto">
-            <Table className="">
-              <TableHeader>
-                <TableRow>
-                  <TableHead className="border-r-2">First Name</TableHead>
-                  <TableHead className="border-r-2">Last Name</TableHead>
-                  <TableHead>Email</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody className="text-black">
-                {attendees.map((a) => (
-                  <TableRow>
-                    <TableCell className="border-r-2">{a.firstName}</TableCell>
-                    <TableCell className="border-r-2">{a.lastName}</TableCell>
-                    <TableCell className="text-[#5C764B] underline ">
-                      <a href={`mailto:${a.email}`}>{a.email}</a>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </div>
-        </div>
-      </div>
-    </>
+        </DialogHeader>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="border-r-2">First Name</TableHead>
+              <TableHead className="border-r-2">Last Name</TableHead>
+              <TableHead>Email</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody className="text-black">
+            {attendees.map((a) => (
+              <TableRow>
+                <TableCell className="border-r-2">{a.firstName}</TableCell>
+                <TableCell className="border-r-2">{a.lastName}</TableCell>
+                <TableCell>{a.email}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </DialogContent>
+    </Dialog>
   );
 }
