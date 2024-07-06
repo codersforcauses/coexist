a85e56fa736dc56bfc5ecdd87de6fcdf45207865
Divrr <96827300+Divrr@users.noreply.github.com>
2024-07-06 02:43:34 +0000

    added nested routing

--- server/api/event/urls.py
+++ server/api/event/urls.py
@@ -1,10 +1,15 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers  # requires drf_nested_routers
 from . import views
 
 router = DefaultRouter()
-router.register(r"", views.EventViewSet, basename="event")
+router.register(r'', views.EventViewSet, basename='event')
+
+event_router = routers.NestedDefaultRouter(router, r'', lookup='event_pk')
+event_router.register(r'rsvps', views.RSVPViewSet, basename='event-rsvps')
 
 urlpatterns = [
-    path("", include(router.urls))
+    path('', include(router.urls)),
+    path('', include(event_router.urls)),
 ]
