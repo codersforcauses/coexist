08e334b117f4b2692cb827c77c3a7c3e04b4b47f
Divrr <96827300+Divrr@users.noreply.github.com>
2024-07-06 01:13:40 +0000

    Added rsvp admin

--- server/api/event/admin.py
+++ server/api/event/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin  # noqa
 
 from .models import Event
 from .models import RSVP
-# Register your models here.
+
+
+class RSVPAdmin(admin.ModelAdmin):
+    list_display = ["user", "event", "created_at", "updated_at"]
+    fields = ["user", "event"]
+
+
 admin.site.register(Event)
-admin.site.register(RSVP)
+admin.site.register(RSVP, RSVPAdmin)
