b2feb264ea585308ba135a724eb29833b7f3c086
Jason Keo <163387808+jasonkeo@users.noreply.github.com>
2024-07-06 07:15:02 +0000

    made success event simpler

--- client/src/components/ui/success-event.tsx
+++ client/src/components/ui/success-event.tsx
@@ -17,35 +17,25 @@ interface prop {
 }
 
 export default function SuccessEvent({ success }: prop) {
-  const [isOpen, setIsOpen] = useState(false);
-
-  useEffect(() => {
-    if (success) {
-      setIsOpen(true);
-    }
-  }, [success]);
-
   return (
-    <div>
-      <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Event Created</AlertDialogTitle>
-          </AlertDialogHeader>
-          <AlertDialogDescription>
-            Your event has been created successfully.
-          </AlertDialogDescription>
-          <AlertDialogFooter>
-            <AlertDialogCancel
-              onClick={() => {
-                window.location.reload();
-              }}
-            >
-              Cancel
-            </AlertDialogCancel>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-    </div>
+    <AlertDialog open={success}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Event Created</AlertDialogTitle>
+        </AlertDialogHeader>
+        <AlertDialogDescription>
+          Your event has been created successfully.
+        </AlertDialogDescription>
+        <AlertDialogFooter>
+          <AlertDialogCancel
+            onClick={() => {
+              window.location.reload();
+            }}
+          >
+            Cancel
+          </AlertDialogCancel>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
