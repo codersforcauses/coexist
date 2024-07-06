fd9a278a99888de1f763f0b205cb6887ad4f0fcb
SetroZ <joemosabry@gmail.com>
2024-07-06 17:05:56 +0800

    used app colors

--- client/src/pages/profile/index.tsx
+++ client/src/pages/profile/index.tsx
@@ -1,11 +1,12 @@
 import { Pen } from "lucide-react";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 
 function EventCount({ title, count }: { title: string; count: number }) {
   return (
     <div className="flex flex-col items-center gap-2 md:gap-5">
-      <p className="">{title}</p>
+      <p className="text-accent">{title}</p>
       <p className="text-5xl">{count}</p>
     </div>
   );
@@ -14,7 +15,7 @@ function CityStatus({ title, value }: { title: string; value: string }) {
   return (
     <>
       <p className="font-medium">{title}</p>
-      <p className="font-medium text-lime-700">{value}</p>
+      <p className="font-medium text-accent">{value}</p>
     </>
   );
 }
@@ -31,11 +32,11 @@ export default function Settings() {
       "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png",
   };
   return (
-    <div className="flex flex-1 flex-col items-center gap-4 bg-gray-200 px-2 py-4 md:px-6 lg:items-start">
+    <div className="flex flex-1 flex-col items-center gap-4 px-2 py-4 md:px-6 lg:items-start">
       <h1 className="text-left text-2xl font-semibold md:text-3xl">
         My Profile
       </h1>
-      <div className="flex w-full flex-grow justify-center rounded-lg bg-lime-700 p-5">
+      <div className="flex w-full flex-grow justify-center rounded-lg bg-accent p-5">
         <div className="flex flex-1 flex-col items-center justify-between gap-5 rounded-lg bg-white px-4 py-6 md:justify-start md:gap-14 md:pt-10 lg:items-start lg:px-10">
           <div className="flex flex-col items-center gap-4 lg:flex-row lg:gap-10">
             <Avatar className="h-44 w-44 md:h-64 md:w-64">
@@ -46,10 +47,10 @@ export default function Settings() {
               <div className="flex items-center gap-2 text-4xl font-semibold md:text-5xl lg:gap-4">
                 <p> {user.name}</p>
                 <button>
-                  <Pen className="h-7 w-7 text-lime-500" />
+                  <Pen className="h-7 w-7 text-accent" />
                 </button>
               </div>
-              <p className="text-green-500 md:text-2xl">{user.email}</p>
+              <p className="md:text-2xl">{user.email}</p>
               <div className="mt-5 grid grid-cols-2 gap-x-7 gap-y-2 text-xl md:mt-7 md:text-2xl">
                 <CityStatus title="Main City" value={user.city} />
                 <CityStatus title="Status" value={user.status} />
@@ -58,13 +59,17 @@ export default function Settings() {
           </div>
 
           <div className="flex justify-between gap-4 text-[1.7rem] md:gap-8 md:text-4xl lg:self-center">
-            <EventCount title="Attended" count={user.attended} />
+            <EventCount title={"Attended"} count={user.attended} />
             <hr className="h-28 w-[1px] rounded-lg bg-gray-600" />
-            <EventCount title="Organised" count={user.organised} />
+            <EventCount title={"Organised"} count={user.organised} />
           </div>
-          <button className="rounded-lg border-2 px-3 py-1 text-2xl md:p-4 md:text-3xl lg:self-center">
-            Sign out
-          </button>
+          <Button
+            size={"lg"}
+            variant={"outline"}
+            className="border-accent text-2xl text-accent lg:self-center"
+          >
+            Sign Out
+          </Button>
         </div>
       </div>
     </div>
