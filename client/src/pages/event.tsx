776617953a111b7100839d20b3cf8dedf0a44d41
Harry Rigg <riggh@icloud.com>
2024-07-13 12:04:28 +0800

    Make page responsive

--- client/src/pages/event.tsx
+++ client/src/pages/event.tsx
@@ -17,23 +17,25 @@ export default function Event() {
   };
 
   return (
-    <div className="m-3 rounded-lg bg-[#9DAD93] p-6">
-      <div className="flex flex-col items-center rounded-lg bg-white px-8 py-4">
-        <div className="text-2xl font-semibold tracking-tight">
+    <div className="m-3 h-full rounded-lg bg-[#9DAD93] p-3 md:p-6">
+      <div className="flex h-full flex-col items-center rounded-lg bg-white px-8 py-4">
+        <div className="text-center text-2xl font-semibold tracking-tight">
           {event.title}
         </div>
         <div className="my-2 w-full border-t border-black"></div>
-        <div className="flex max-w-[35%] flex-col gap-2">
+        <div className="flex w-full flex-col gap-2 sm:max-w-[80%] md:max-w-[60%] xl:max-w-[35%]">
           <div className="text-center">{event.description}</div>
-          <div className="flex gap-3">
+          <div className="flex flex-col items-center justify-center gap-1 md:flex-row md:items-start md:gap-3">
             <div className="font-bold">Bring:</div>
-            <div>{event.what_to_bring}</div>
+            <div className="text-center md:text-left">
+              {event.what_to_bring}
+            </div>
           </div>
         </div>
 
-        <div className="my-5 grid h-[400px] w-full grid-cols-[1fr,1px,1fr]">
-          <div className="mr-10 flex flex-col items-end justify-center">
-            <div className="relative h-[300px] w-[500px]">
+        <div className="my-5 grid h-full w-full grid-rows-[1fr,1px,1fr] md:grid-cols-[1fr,1px,1fr] md:grid-rows-[500px]">
+          <div className="mb-5 flex flex-col items-center justify-center md:mb-0 md:mr-10 md:items-end">
+            <div className="relative h-full max-h-[300px] w-full max-w-[500px]">
               <Image
                 fill={true}
                 src={event.image_href}
@@ -42,12 +44,12 @@ export default function Event() {
               />
             </div>
           </div>
-          <div className="border-l border-black"></div>
-          <div className="ml-10 flex flex-col justify-center gap-4">
+          <div className="border-t border-black md:border-l"></div>
+          <div className="mt-5 flex flex-col justify-center gap-4 md:ml-10 md:mt-0">
             <div className="flex flex-col gap-1">
               <span className="font-semibold">Location:</span>
               <div className="flex gap-3">
-                <div className="rounded bg-[#9DAD93] px-2 text-white">
+                <div className="self-start rounded bg-[#9DAD93] px-2 text-white">
                   {event.branch_name}
                 </div>
                 <span>{event.location}</span>
