9d1b912783bd41ae82cf54fa8108c4fbe6291056
Divrr <96827300+Divrr@users.noreply.github.com>
2024-07-13 00:04:41 +0000

    Added APITestCase for get/post/put/patch/delete

--- server/api/branch/tests.py
+++ server/api/branch/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
 from .models import Branch
 
 
@@ -33,3 +34,59 @@ class SoftDeleteTestCase(TestCase):
         self.branch.delete()
         self.branch2.delete()
         self.assertEqual(len(Branch.objects_deleted.all()), 2)
+
+
+class BranchTestCase(APITestCase):
+    def setUp(self):
+        self.branch = Branch.objects.create(name="Test1", description="Desc1")
+        self.branch2 = Branch.objects.create(name="Test2", description="Desc2")
+        self.branch3 = Branch.objects.create(name="Test3", description="Desc3")
+
+    def test_get_all_branches(self):
+        response = self.client.get('/api/branch/')
+        self.assertEqual(response.status_code, 200)
+
+        self.assertIn('count', response.data)
+        self.assertIn('next', response.data)
+        self.assertIn('previous', response.data)
+        self.assertIn('results', response.data)
+
+        self.assertEqual(response.data['count'], 3)
+
+    def test_get_single_branch(self):
+        response = self.client.get(f'/api/branch/{self.branch.id}/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['name'], 'Test1')
+
+    def test_post_branch(self):
+        response = self.client.post('/api/branch/', {
+            'name': 'Test4',
+            'description': 'Desc4'
+        })
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data['name'], 'Test4')
+
+    def test_put_branch(self):
+        response = self.client.put(f'/api/branch/{self.branch.id}/', {
+            'name': 'Test5',
+            'description': 'Desc5'
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['name'], 'Test5')
+
+    def test_patch_branch(self):
+        response = self.client.patch(f'/api/branch/{self.branch.id}/', {
+            'name': 'Test6'
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['name'], 'Test6')
+
+    def test_delete_branch(self):
+        response = self.client.delete(f'/api/branch/{self.branch.id}/')
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(len(Branch.objects.all()), 2)
+        self.assertEqual(len(Branch.objects_deleted.all()), 1)
+        self.assertEqual(Branch.objects_deleted.all()[0].name, 'Test1')
+        self.branch.restore()
+        self.assertEqual(len(Branch.objects.all()), 3)
+        self.assertEqual(len(Branch.objects_deleted.all()), 0)
