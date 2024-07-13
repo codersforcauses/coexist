ee799a94a5d0579371be12a9b65872137a503430
Connor2803 <87635341+Connor2803@users.noreply.github.com>
2024-07-13 07:28:35 +0000

    Add phone number field and migrations Added the phone number field and associated migration for it. Will go and add it to my /users/me endpoint once this is merged too.

--- server/api/users/models.py
+++ server/api/users/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User, Group
 
 class ExtendedUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=15, blank=True)
 
     def __str__(self):
         return self.user.username
