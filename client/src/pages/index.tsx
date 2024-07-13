5bc827c4a00f05fe0be6c2eac310bcfd6eebcc2f
Zarif <23640056@student.uwa.edu.au>
2024-07-13 03:30:41 +0000

    import RsvpListModal

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+import RsvpListModal from "@/components/main/RsvpListModal";
 import { usePings } from "@/hooks/pings";
 
 import { Button } from "../components/ui/button";
@@ -24,6 +25,7 @@ export default function Home() {
       <p>
         Response from server: <span>{data as string}</span>
       </p>
+      <RsvpListModal />
     </div>
   );
 }
