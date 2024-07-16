90b45adf48d2555203342943dde1a5e4ebd0f653
SetroZ <joemosabry@gmail.com>
2024-07-16 08:45:30 +0800

    fixed long line

--- server/api/event/migrations/0008_event_location_url.py
+++ server/api/event/migrations/0008_event_location_url.py
@@ -6,14 +6,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('event', '0007_merge_20240710_0852'),
+        ("event", "0007_merge_20240710_0852"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='event',
-            name='location_url',
-            field=models.URLField(default='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15270.158808639862!2d145.7088778!3d-16.8986496!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x697864fe8ee140d5%3A0xf00eef26261dd60!2sGlenoma%20Park!5e0!3m2!1sen!2sau!4v1721025698144!5m2!1sen!2sau'),
+            model_name="event",
+            name="location_url",
+            field=models.URLField(
+                default="""https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15270.
+                                  158808639862!2d145.7088778!3d-16.8986496!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x697864fe8ee140d5%3A
+                                  0xf00eef26261dd60!2sGlenoma%20Park!5e0!3m2!1sen!2sau!4v1721025698144!5m2!1sen!2sau"""
+            ),
             preserve_default=False,
         ),
     ]
