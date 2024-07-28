5d29fb903f938293250bc61945ae4cdb1b370362
Harry Rigg <riggh@icloud.com>
2024-07-28 23:32:52 +0800

    Improve signup/login modal functionality

--- client/src/components/ui/SignUpModal.tsx
+++ client/src/components/ui/SignUpModal.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { ReactNode } from "react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -20,14 +21,13 @@ import {
 } from "@/components/ui/select";
 
 interface Props {
-  isOpen: boolean;
-  onClose: () => void;
+  children: ReactNode;
 }
 
-function SignUpModal({ isOpen, onClose }: Props) {
+function SignUpModal({ children }: Props) {
   return (
-    <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogTrigger asChild></DialogTrigger>
+    <Dialog>
+      <DialogTrigger asChild>{children}</DialogTrigger>
       {/* Outer green container */}
       <DialogContent
         className="mx-auto flex h-full max-h-[725px] w-[95%] max-w-[600px] flex-col border-0 bg-accent p-[8px] shadow-lg"
